11452196e72554739ceccdb13f34c3871239d545
Tamekia <tamekialnelson@gmail.com>
2026-05-02 23:18:34 -0500

    fix nav

--- frontend/src/components/layout/NavBar.jsx
+++ frontend/src/components/layout/NavBar.jsx
@@ -5,13 +5,14 @@ export default function Navbar() {
     <nav className="navbar">
 
       <div className="nav-left">
-        <h2>Flavor Savor</h2>
+        <NavLink to="/" className="logo-link">
+          <h2>Flavor Savor</h2>
+        </NavLink>
       </div>
 
       <div className="nav-links">
         <NavLink to="/restaurants">Restaurants</NavLink>
         <NavLink to="/favorites">Favorites</NavLink>
-        <NavLink to="/users">Users</NavLink>
       </div>
 
     </nav>
